9007bc874145a538b83d406c89f79d702438a523
Martin Schade <schadem@amazon.com>
2019-12-19 20:50:04 -0800

    wip camera

--- tasks/human_pose/demo_python_script.py
+++ tasks/human_pose/demo_python_script.py
@@ -1,87 +1,116 @@
-# load human pose task
-import json
-import trt_pose.coco
-
-with open('human_pose.json', 'r') as f:
-    human_pose = json.load(f)
-
-topology = trt_pose.coco.coco_category_to_topology(human_pose)
-
-# load model with heatmap channels and part affinity field channels
-import trt_pose.models
-
-num_parts = len(human_pose['keypoints'])
-num_links = len(human_pose['skeleton'])
-
-model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
-
-# load weights
-import torch
-
-MODEL_WEIGHTS = 'resnet18_baseline_att_224x224_A_epoch_249.pth'
-
-model.load_state_dict(torch.load(MODEL_WEIGHTS))
-
-# to optimize for TensorRT
+# # load human pose task
+# import json
+# import trt_pose.coco
+#
+# with open('human_pose.json', 'r') as f:
+#     human_pose = json.load(f)
+#
+# topology = trt_pose.coco.coco_category_to_topology(human_pose)
+#
+# # load model with heatmap channels and part affinity field channels
+# import trt_pose.models
+#
+# num_parts = len(human_pose['keypoints'])
+# num_links = len(human_pose['skeleton'])
+#
+# model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
+#
+# # load weights
+# import torch
+#
+# MODEL_WEIGHTS = 'resnet18_baseline_att_224x224_A_epoch_249.pth'
+#
+# model.load_state_dict(torch.load(MODEL_WEIGHTS))
+#
+# # to optimize for TensorRT
 WIDTH = 224
 HEIGHT = 224
-
-data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
-
-# optimize the model
-import torch2trt
-
-model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
-
-# optimized model
-OPTIMIZED_MODEL = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
-
-torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
-
-
-from torch2trt import TRTModule
-
-model_trt = TRTModule()
-model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
-
-
-# benchmark
-
-import time
-
-t0 = time.time()
-torch.cuda.current_stream().synchronize()
-for i in range(50):
-    y = model_trt(data)
-torch.cuda.current_stream().synchronize()
-t1 = time.time()
-
-print(50.0 / (t1 - t0))
-
-# process image (BGR8 / HWC)
+#
+# data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+#
+# # optimize the model
+# import torch2trt
+#
+# model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1 << 25)
+#
+# # optimized model
+# OPTIMIZED_MODEL = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
+#
+# torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
+#
+# from torch2trt import TRTModule
+#
+# model_trt = TRTModule()
+# model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
+#
+# # benchmark
+#
+# import time
+#
+# t0 = time.time()
+# torch.cuda.current_stream().synchronize()
+# for i in range(50):
+#     y = model_trt(data)
+# torch.cuda.current_stream().synchronize()
+# t1 = time.time()
+#
+# print(50.0 / (t1 - t0))
+#
+# # process image (BGR8 / HWC)
 import cv2
-import torchvision.transforms as transforms
-import PIL.Image
+# import torchvision.transforms as transforms
+# import PIL.Image
+#
+# mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
+# std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+# device = torch.device('cuda')
+#
+#
+# def preprocess(image):
+#     global device
+#     device = torch.device('cuda')
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     image = PIL.Image.fromarray(image)
+#     image = transforms.functional.to_tensor(image).to(device)
+#     image.sub_(mean[:, None, None]).div_(std[:, None, None])
+#     return image[None, ...]
+#
+#
+# # parse objects from inference
+# from trt_pose.draw_objects import DrawObjects
+# from trt_pose.parse_objects import ParseObjects
+#
+# parse_objects = ParseObjects(topology)
+# draw_objects = DrawObjects(topology)
+#
+# # from jetcam.usb_camera import USBCamera
+from jetcam.csi_camera import CSICamera
+# from jetcam.utils import bgr8_to_jpeg
+#
+# # camera = USBCamera(width=WIDTH, height=HEIGHT, capture_fps=30)
+camera = CSICamera(width=WIDTH, height=HEIGHT, capture_fps=30)
+#
+# camera.running = True
+#
+# import ipywidgets
+# from IPython.display import display
+#
+# image_w = ipywidgets.Image(format='jpeg')
+#
+# display(image_w)
+# import cv2
 
-mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
-std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
-device = torch.device('cuda')
+def execute(change):
+    image = change['new']
+    # data = preprocess(image)
+    # cmap, paf = model_trt(data)
+    # cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
+    # counts, objects, peaks = parse_objects(cmap, paf)  # , cmap_threshold=0.15, link_threshold=0.15)
+    # draw_objects(image, counts, objects, peaks)
+    cv2.imshow('Frame', image)
 
-def preprocess(image):
-    global device
-    device = torch.device('cuda')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = PIL.Image.fromarray(image)
-    image = transforms.functional.to_tensor(image).to(device)
-    image.sub_(mean[:, None, None]).div_(std[:, None, None])
-    return image[None, ...]
+execute({'new': camera.value})
 
-# parse objects from inference
-from trt_pose.draw_objects import DrawObjects
-from trt_pose.parse_objects import ParseObjects
+camera.observe(execute, names='value')
 
-parse_objects = ParseObjects(topology)
-draw_objects = DrawObjects(topology)
-
-
-
+camera.unobserve_all()
